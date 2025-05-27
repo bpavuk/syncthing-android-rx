5939a300238eb102ff426d4b53816ea17b32f2c2
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-05-27 23:34:36 +0300

    moved Compose UI package where it was initially supposed to be

--- app/src/main/java/com/nutomic/syncthingandroid/ui/theme/Type.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.nutomic.syncthingandroid.activities.ui.theme
+package com.nutomic.syncthingandroid.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
