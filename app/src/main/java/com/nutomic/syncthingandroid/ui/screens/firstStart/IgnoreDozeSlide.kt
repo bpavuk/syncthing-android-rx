d11541134df60baf03328249be9235bb12358019
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-05-28 14:32:46 +0300

    shuffling files around

---
+++ app/src/main/java/com/nutomic/syncthingandroid/ui/screens/firstStart/IgnoreDozeSlide.kt
@@ -0,0 +1,35 @@
+package com.nutomic.syncthingandroid.ui.screens.firstStart
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nutomic.syncthingandroid.ui.theme.SyncthingandroidTheme
+
+@Composable
+fun IgnoreDozePermissionSlide(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primaryContainer) then modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+    }
+}
+
+@Preview
+@Composable
+private fun IgnoreDozePermissionPreview() {
+    SyncthingandroidTheme {
+        IgnoreDozePermissionSlide(
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
