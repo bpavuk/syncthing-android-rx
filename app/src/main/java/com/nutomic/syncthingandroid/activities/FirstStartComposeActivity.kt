005687a84ced121ba9ab5b498fc876c2f2f8ce0f
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-05-28 21:19:10 +0300

    implemented location permission screen

--- app/src/main/java/com/nutomic/syncthingandroid/activities/FirstStartComposeActivity.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/activities/FirstStartComposeActivity.kt
@@ -1,26 +1,17 @@
 package com.nutomic.syncthingandroid.activities
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.ActivityNotFoundException
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.os.PowerManager
-import android.provider.Settings
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.net.toUri
-import com.nutomic.syncthingandroid.R
 import com.nutomic.syncthingandroid.service.Constants
 import com.nutomic.syncthingandroid.ui.screens.FirstStartScreen
 import com.nutomic.syncthingandroid.ui.screens.Slide
@@ -34,7 +25,6 @@ class FirstStartComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val showSlideIgnoreDozePermission = !haveIgnoreDozePermission()
         val showSlideLocationPermission = !haveLocationPermission()
         val showSlideNotificationPermission = !haveNotificationPermission()
         val showSlideKeyGeneration = !checkForParseableConfig()
@@ -59,47 +49,6 @@ class FirstStartComposeActivity : ComponentActivity() {
         }
     }
 
-    private fun haveIgnoreDozePermission(): Boolean {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            // Older android version don't have the doze feature so we'll assume having the anti-doze permission.
-            return true
-        }
-        val pm = getSystemService(POWER_SERVICE) as PowerManager
-        return pm.isIgnoringBatteryOptimizations(packageName)
-    }
-
-    @SuppressLint("InlinedApi")
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun requestIgnoreDozePermission() {
-        val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
-        intent.setData("package:$packageName".toUri())
-        try {
-            val componentName = intent.resolveActivity(packageManager)
-            if (componentName != null) {
-                val className = componentName.className
-                if (!className.equals(
-                        "com.android.tv.settings.EmptyStubActivity",
-                        ignoreCase = true
-                    )
-                ) {
-                    // Launch "Exempt from doze mode?" dialog.
-                    startActivity(intent)
-                    return
-                }
-            } else {
-                Log.w(TAG, "Request ignore battery optimizations not supported")
-            }
-        } catch (e: ActivityNotFoundException) {
-            Log.w(TAG, "Request ignore battery optimizations not supported", e)
-        }
-
-        Toast.makeText(
-            this,
-            R.string.dialog_disable_battery_optimizations_not_supported,
-            Toast.LENGTH_LONG
-        ).show()
-    }
-
     private fun haveLocationPermission(): Boolean {
         val coarseLocationGranted = ContextCompat.checkSelfPermission(
             this,
@@ -116,31 +65,32 @@ class FirstStartComposeActivity : ComponentActivity() {
     }
 
     private fun requestLocationPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            ActivityCompat.requestPermissions(
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    REQUEST_FINE_LOCATION
+                )
+            }
+            Build.VERSION.SDK_INT == Build.VERSION_CODES.Q -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    ),
+                    REQUEST_BACKGROUND_LOCATION
+                )
+            }
+            else -> ActivityCompat.requestPermissions(
                 this,
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ),
-                REQUEST_FINE_LOCATION
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                REQUEST_COARSE_LOCATION
             )
-            return
-        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                ),
-                REQUEST_BACKGROUND_LOCATION
-            )
-            return
         }
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-            REQUEST_COARSE_LOCATION
-        )
     }
 
     private fun haveNotificationPermission(): Boolean {
