b5543f90066033d1773501dfad6781693e13d091
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-05-28 18:32:07 +0300

    refactored doze slide a bit so that Android internals don't get into the way

--- app/src/main/java/com/nutomic/syncthingandroid/ui/screens/firstStart/IgnoreDozeSlide.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/ui/screens/firstStart/IgnoreDozeSlide.kt
@@ -1,13 +1,6 @@
 package com.nutomic.syncthingandroid.ui.screens.firstStart
 
-import android.content.Context
-import android.content.Intent
 import android.os.Build
-import android.os.PowerManager
-import android.provider.Settings
-import android.util.Log
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,20 +10,15 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.net.toUri
 import com.nutomic.syncthingandroid.R
 import com.nutomic.syncthingandroid.ui.common.slides.SlideState
 import com.nutomic.syncthingandroid.ui.common.slides.rememberSlideState
 import com.nutomic.syncthingandroid.ui.theme.SyncthingandroidTheme
+import com.nutomic.syncthingandroid.util.compose.doze.rememberDozePermissionState
 
 @Composable
 fun IgnoreDozePermissionSlide(modifier: Modifier = Modifier, slideState: SlideState) {
@@ -56,26 +44,15 @@ fun IgnoreDozePermissionSlide(modifier: Modifier = Modifier, slideState: SlideSt
                 slideState.forwardBlocked = false
                 // All set!
             } else {
-                val packageName = LocalContext.current.packageName
-                val powerManager = LocalContext.current.getSystemService(Context.POWER_SERVICE) as PowerManager
-                var dozePermissionGranted by remember { mutableStateOf(powerManager.isIgnoringBatteryOptimizations(packageName)) }
+                val dozePermissionState = rememberDozePermissionState()
 
-                val requestDozePermission = rememberLauncherForActivityResult(
-                    contract = ActivityResultContracts.StartActivityForResult()
-                ) { result ->
-                    dozePermissionGranted = powerManager.isIgnoringBatteryOptimizations(packageName)
-                    Log.d("IgnoreDozeSlide", result.data.toString())
-                }
-
-                if (dozePermissionGranted) {
+                if (dozePermissionState.granted) {
                     // All set!
                     Text("Battery permissions are set")
                     slideState.forwardBlocked = false
                 } else {
-                    val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
-                    intent.setData("package:$packageName".toUri())
                     Text("Please grant battery permissions")
-                    Button(onClick = { requestDozePermission.launch(intent) }) {
+                    Button(onClick = { dozePermissionState.requestPermission() }) {
                         Text(stringResource(R.string.grant_permission))
                     }
                 }
