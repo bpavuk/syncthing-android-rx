32a29d46bc1b9828335a61be4059dbb916350b84
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-05-28 21:43:17 +0300

    implemented notification permission screen

--- app/src/main/java/com/nutomic/syncthingandroid/ui/screens/firstStart/NotificationPermissionSlide.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/ui/screens/firstStart/NotificationPermissionSlide.kt
@@ -1,18 +1,29 @@
 package com.nutomic.syncthingandroid.ui.screens.firstStart
 
+import android.Manifest
+import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.nutomic.syncthingandroid.R
 import com.nutomic.syncthingandroid.ui.common.slides.SlideState
 import com.nutomic.syncthingandroid.ui.common.slides.rememberSlideState
 import com.nutomic.syncthingandroid.ui.theme.SyncthingandroidTheme
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun NotificationPermissionSlide(modifier: Modifier = Modifier, slideState: SlideState) {
     Column(
@@ -22,7 +33,39 @@ fun NotificationPermissionSlide(modifier: Modifier = Modifier, slideState: Slide
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        slideState.forwardBlocked = true
 
+        Box(
+            modifier = Modifier.weight(0.3f),
+            contentAlignment = Alignment.Center
+        ) {
+            // Notification permissions
+        }
+        Column(
+            modifier = Modifier.weight(0.7f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> {
+                    val notificationPermission = rememberPermissionState(
+                        permission = Manifest.permission.POST_NOTIFICATIONS
+                    )
+                    if (notificationPermission.status.isGranted) {
+                        Text("Enabled!")
+                    } else {
+                        Text(stringResource(R.string.notification_permission_desc))
+                        Button(onClick = {
+                            notificationPermission.launchPermissionRequest()
+                        }) {
+                            Text(stringResource(R.string.grant_permission))
+                        }
+                    }
+                }
+                else -> {
+                    Text("No notification permission required on this device.")
+                }
+            }
+        }
     }
 }
 
