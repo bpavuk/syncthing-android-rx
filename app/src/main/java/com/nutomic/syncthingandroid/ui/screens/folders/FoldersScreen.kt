69e90fdcb746aaf8b0fbc40fad85e5f75f41ffe3
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-06-01 12:43:40 +0300

    kinda reorganized

---
+++ app/src/main/java/com/nutomic/syncthingandroid/ui/screens/folders/FoldersScreen.kt
@@ -0,0 +1,9 @@
+package com.nutomic.syncthingandroid.ui.screens.folders
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FoldersScreen(modifier: Modifier = Modifier) {
+
+}
