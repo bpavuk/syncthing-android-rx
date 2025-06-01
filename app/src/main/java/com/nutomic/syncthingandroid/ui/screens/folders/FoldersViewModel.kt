374878e19b23a66a127aa512b0dbdf0fcdaf38cf
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-06-01 13:00:38 +0300

    initial implementation of FoldersViewModel

--- app/src/main/java/com/nutomic/syncthingandroid/ui/screens/folders/FoldersViewModel.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/ui/screens/folders/FoldersViewModel.kt
@@ -1,4 +1,40 @@
 package com.nutomic.syncthingandroid.ui.screens.folders
 
-class FoldersViewModel {
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.nutomic.syncthingandroid.model.Folder
+import com.nutomic.syncthingandroid.service.RestApi
+import com.nutomic.syncthingandroid.util.ConfigRouter
+import com.nutomic.syncthingandroid.util.ConfigXml
+
+interface FoldersViewModel {
+    val folders: List<Folder>
+
+    fun retrieveFolders()
+}
+
+class FoldersViewModelImpl(
+    private val configRouter: ConfigRouter,
+    private val restApi: RestApi
+) : FoldersViewModel, ViewModel() {
+    override var folders: List<Folder> by mutableStateOf(emptyList())
+        private set
+
+    override fun retrieveFolders() {
+        try {
+            folders = configRouter.getFolders(restApi)
+        } catch (e: ConfigXml.OpenConfigException) {
+            Log.e(
+                TAG,
+                "Failed to parse existing config. You will need support from here...",
+                e
+            )
+        }
+    }
+
+    @Suppress("PrivatePropertyName")
+    private val TAG = "FoldersViewModelImpl"
 }
