b2a6ad0197c9ac3062f3198c6ffe7c63bd964a9b
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-06-02 20:30:37 +0300

    Kotlinized SyncthingApp

--- app/src/main/java/com/nutomic/syncthingandroid/SyncthingApp.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/SyncthingApp.kt
@@ -1,29 +1,46 @@
-package com.nutomic.syncthingandroid;
+package com.nutomic.syncthingandroid
 
-import android.app.Application;
-import android.os.StrictMode;
+import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
+import com.nutomic.syncthingandroid.service.RestApiKt
+import com.nutomic.syncthingandroid.util.ConfigRouter
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import javax.inject.Inject
 
-import javax.inject.Inject;
+// Define your Koin modules here or in a separate file
+val appModule = module {
+    single { ConfigRouter(androidApplication()) }
+    single { RestApiKt() }
+}
 
-public class SyncthingApp extends Application {
+class SyncthingApp : Application() {
+    @Inject
+    lateinit var mComponent: DaggerComponent
 
-    @Inject DaggerComponent mComponent;
+    override fun onCreate() {
+        super.onCreate()
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+        // Initialize Koin
+        startKoin {
+            androidContext(this@SyncthingApp)
+            modules(appModule)
+        }
 
         DaggerDaggerComponent.builder()
-                .syncthingModule(new SyncthingModule(this))
-                .build()
-                .inject(this);
+            .syncthingModule(SyncthingModule(this))
+            .build()
+            .inject(this)
 
         // Set VM policy to avoid crash when sending folder URI to file manager.
-        StrictMode.VmPolicy vmPolicy = new StrictMode.VmPolicy.Builder()
-                .detectAll()
-                .penaltyLog()
-                .build();
-        StrictMode.setVmPolicy(vmPolicy);
+        val vmPolicy = VmPolicy.Builder()
+            .detectAll()
+            .penaltyLog()
+            .build()
+        StrictMode.setVmPolicy(vmPolicy)
 
         /*
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P)
@@ -36,7 +53,7 @@ public class SyncthingApp extends Application {
         */
     }
 
-    public DaggerComponent component() {
-        return mComponent;
+    fun component(): DaggerComponent {
+        return mComponent
     }
 }
