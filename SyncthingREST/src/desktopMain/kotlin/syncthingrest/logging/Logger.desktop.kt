923f138d8ba0d69d699a197d72a1d3e03f2f90ad
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-06-05 13:16:13 +0300

    feat: add cross-platform logger for Android and desktop

---
+++ SyncthingREST/src/desktopMain/kotlin/syncthingrest/logging/Logger.desktop.kt
@@ -0,0 +1,41 @@
+package syncthingrest.logging
+
+actual class Logger {
+    private fun log(level: Level, tag: String, message: String, e: Exception?) {
+        println("# $tag @ $level: $message")
+        e?.let { e ->
+            println("Exception: ${e::class.qualifiedName}")
+            e.message?.let { message -> println(message) }
+            println()
+            e.printStackTrace()
+        }
+    }
+
+    actual fun e(tag: String, message: String, e: Exception?) {
+        log(Level.E, tag, message, e)
+    }
+
+    actual fun w(tag: String, message: String, e: Exception?) {
+        log(Level.W, tag, message, e)
+    }
+
+    actual fun i(tag: String, message: String, e: Exception?) {
+        log(Level.I, tag, message, e)
+    }
+
+    actual fun d(tag: String, message: String, e: Exception?) {
+        log(Level.D, tag, message, e)
+    }
+
+    actual fun v(tag: String, message: String, e: Exception?) {
+        log(Level.V, tag, message, e)
+    }
+
+    actual fun wtf(tag: String, message: String, e: Exception?) {
+        log(Level.WTF, tag, message, e)
+    }
+}
+
+private enum class Level {
+    E, W, I, D, V, WTF
+}
